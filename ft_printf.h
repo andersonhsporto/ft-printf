1fa1684bb5f583554f66e50181d5b190b1a6ca61
Anderson Porto <andersonhigo2@gmail.com>
2021-09-11 21:42:36 -0300

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/11 21:39:08 by anhigo-s          #+#    #+#             */
+/*   Updated: 2021/09/11 21:39:09 by anhigo-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 # include <unistd.h>
@@ -7,16 +19,15 @@
 # include <stdio.h>
 # include "./libft/libft.h"
 
+int	ft_printf(const char *format, ...);
+
 int	putchar_fd(char c);
-int putstring_fd(char *s, int fd);
-int putnbr_fd(int n);
+int	putstring_fd(char *s, int fd);
+int	putnbr_fd(int n);
 
 int	unsigned_fd(unsigned int n);
 
 int	hex_str(size_t n, char c);
-
 int	hex_void(size_t n);
-
-int	ft_printf(const char *format, ...);
 
 #endif
