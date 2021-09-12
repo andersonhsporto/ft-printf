cf5e1881e5cf0809d7363179e7a3d62265d468b0
Anderson Porto <andersonhigo2@gmail.com>
2021-09-11 23:43:30 -0300

    Update print_hexadecimal.c

--- src/print_hexadecimal.c
+++ src/print_hexadecimal.c
@@ -1,18 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   print_hexadecimal.c                                :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/11 21:28:47 by anhigo-s          #+#    #+#             */
-/*   Updated: 2021/09/11 21:37:58 by anhigo-s         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "../ft_printf.h"
 
-static int	hex_len(size_t n)
+static int	hex_len(unsigned int n)
 {
 	int	count;
 
@@ -46,15 +34,15 @@ static int	print_hex(char *str, char c)
 	return (i);
 }
 
-int	hex_str(size_t n, char c)
+static int	hex_c(unsigned int n, char c)
 {
 	char			*str;
 	char			*digits;
-	unsigned int	i;
+	int	i;
 
 	i = hex_len(n);
 	digits = "0123456789abcdef";
-	str = malloc(i * sizeof(char));
+	str = ft_calloc((i + 1), sizeof(char));
 	if (!str)
 		return (0);
 	while (i)
@@ -67,12 +55,16 @@ int	hex_str(size_t n, char c)
 	return (i);
 }
 
-int	hex_void(size_t n)
+
+int	hex_str(unsigned int n, char c)
 {
 	int	i;
 
 	i = 0;
-	i += putstring_fd("0x", 1);
-	i += hex_str(n, 'x');
-	return (i);
+	if (n == 0)
+	{
+		i += putchar_fd('0');
+		return (i);
+	}
+	return (hex_c(n, c));
 }
