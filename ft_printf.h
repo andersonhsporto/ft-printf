444a991f7db2f30302871922c07558858c239548
Anderson Porto <andersonhigo2@gmail.com>
2021-09-11 21:15:25 -0300

    Update ft_printf.h

--- ft_printf.h
+++ ft_printf.h
@@ -7,15 +7,15 @@
 # include <stdio.h>
 # include "./libft/libft.h"
 
-int	putchar_fd(char c, int fd);
+int	putchar_fd(char c);
 int putstring_fd(char *s, int fd);
 int putnbr_fd(int n);
 
 int	unsigned_fd(unsigned int n);
 
-int	hex_str(unsigned int n, char c);
+int	hex_str(size_t n, char c);
 
-int	hex_void(unsigned int n);
+int	hex_void(size_t n);
 
 int	ft_printf(const char *format, ...);
 
