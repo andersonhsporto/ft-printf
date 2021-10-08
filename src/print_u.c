7252c4b58555cf388b21d267e059013b97c34a0b
Anderson Porto <andersonhigo2@gmail.com>
2021-10-08 00:09:10 -0300

    Update print_u.c

--- src/print_u.c
+++ src/print_u.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_u.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anhigo-s <anhigo-s@student.42sp.org.br>    +#+  +:+       +#+        */
+/*   By: anhigo-s <anhigo-s@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/12 21:09:42 by anhigo-s          #+#    #+#             */
-/*   Updated: 2021/09/12 21:09:43 by anhigo-s         ###   ########.fr       */
+/*   Updated: 2021/10/08 00:07:24 by anhigo-s         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,18 @@
 int	unsigned_fd(unsigned int n)
 {
 	unsigned int	i;
-	int				j;
+	static int		j;
 
 	i = n;
-	j = 0;
+	if (!j)
+		j = 0;
 	if (i > 9)
 	{
 		unsigned_fd(i / 10);
 	}
 	ft_putchar_fd((i % 10) + '0', 1);
+	j++;
 	if (n == 0)
 		return (1);
-	while (n)
-	{
-		n /= 10;
-		j++;
-	}
 	return (j);
 }
